f549673ee122c5bebcf3da3185a3aaa5c7b2911b
Arun Kumar <arunkumar.official35@gmail.com>
2026-01-25 07:26:29 +0530

    Add Divisibility counter.py file

--- Divisibility counter.py
+++ Divisibility counter.py
@@ -1,3 +1,4 @@
+# Program to count how many elements in a list are divisible by another list of numbers.
 a = [1,2,8,9,12,46,76,82,15,20,30]
 b= [1,2,3,4,5,6,7,8,9]
 dic={}
@@ -9,4 +10,5 @@ for i in b :
             count+=1
     dic[i] = count
     count=0
+
 print(dic)
